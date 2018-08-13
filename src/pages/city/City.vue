45be4073ca960ae77ae380c8aa104e3c7cc8cc87
郁世昌 <yushichang0845@dingtalk.com>
2018-08-13 13:58:28 +0800

    城市选择页面进度提交

---
+++ src/pages/city/City.vue
@@ -0,0 +1,32 @@
+<template lang='pug'>
+  .city
+    CityHeader
+    CitySearch
+</template>
+
+<script>
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+export default {
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch
+  },
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang='stylus'>
+
+</style>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,41 @@
+<template lang='pug'>
+  .search
+    input.search-input(type="text" placeholder="输入城市名或拼音")
+</template>
+
+<script>
+export default {
+  name: 'CitySearch',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang='stylus'>
+  @import '~styles/varibles.styl';
+  .search
+    padding 0 .05rem
+    height .36rem
+    background-color $bgColor
+    .search-input
+      box-sizing border-box
+      padding 0 .1rem
+      height .32rem
+      line-height .32rem
+      width 100%
+      text-align center
+      border-radius .03rem
+      color #666
+</style>
